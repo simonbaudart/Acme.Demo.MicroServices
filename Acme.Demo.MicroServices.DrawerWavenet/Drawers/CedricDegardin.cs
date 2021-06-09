bf0dbc3b517878a07b97c7068df65b6d458e41b8
Simon Baudart <s.baudart@wavenet.be>
2021-06-09 14:25:06 +0200

    Upgrade CedricDegardin

--- Acme.Demo.MicroServices.DrawerWavenet/Drawers/CedricDegardin.cs
+++ Acme.Demo.MicroServices.DrawerWavenet/Drawers/CedricDegardin.cs
@@ -13,21 +13,31 @@ namespace Acme.Demo.MicroServices.DrawerWavenet.Drawers
 
     public class CedricDegardin : IDrawPicture
     {
+        private static readonly Random Random = new Random(DateTime.Now.Millisecond);
+
         public Bitmap Draw(int height, int width)
         {
-            var random = new Random(DateTime.Now.Millisecond);
+            height = Math.Clamp(height, 1000, 6000);
+            width = Math.Clamp(width, 1000, 6000);
+
             var picture = new Bitmap(height, width);
 
-            for (int i = 0; i < picture.Width; i++)
+            for (var i = 0; i < picture.Width; i++)
             {
-                for (int j = 0; j < picture.Height; j++)
+                for (var j = 0; j < picture.Height; j++)
                 {
-                    var pixelColor = Color.FromArgb(random.Next(1, 256), random.Next(1, 256), random.Next(1, 256));
+                    var pixelColor = RandomPixel(i, j);
                     picture.SetPixel(i, j, pixelColor);
                 }
             }
 
             return picture;
         }
+
+        private static Color GradiantPixel(int i, int j)
+            => Color.FromArgb(Math.Clamp(i, 1, 255), Math.Clamp(j, 1, 25), Math.Clamp(i * j, 1, 255));
+
+        private static Color RandomPixel(int i, int j)
+            => Color.FromArgb(Random.Next(1, 255), Random.Next(1, 255), Random.Next(1, 255));
     }
 }
